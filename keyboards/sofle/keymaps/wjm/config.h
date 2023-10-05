bfa3a36aee20fa66bd88ac1460d19743fcfe87e2
Whitney Marshall <whitney.marshall@gmail.com>
2023-10-04 19:48:33 -0500

    Initial customizations for my new Sofle keyboard

---
+++ keyboards/sofle/keymaps/wjm/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+#define DYNAMIC_TAPPING_TERM_INCREMENT 10
+#define TAPPING_TERM_PER_KEY
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
