99b591a2b8a90c8dd409cd2207e23e2a28bc8f29
Whitney Marshall <whitney.marshall@garmin.com>
2023-10-05 22:24:55 +0100

    Add/update wjm keymap to sofle/rev1

---
+++ keyboards/sofle/keymaps/wjm/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+#define DYNAMIC_TAPPING_TERM_INCREMENT 10
+#define TAPPING_TERM_PER_KEY
+#define TAPPING_TERM 240
+#define PERMISSIVE_HOLD
+#define RETRO_TAPPING
+
+#define ACTION_DEBUG
